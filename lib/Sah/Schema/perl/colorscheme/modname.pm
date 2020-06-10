c3334eb203898fea463bbcde138e3eb1b374e623
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-10 11:26:02 +0700

    Initial commit

---
+++ lib/Sah/Schema/perl/colorscheme/modname.pm
@@ -0,0 +1,13 @@
+package Sah::Schema::perl::colorscheme::modname;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use Sah::PSchema qw(get_schema);
+
+our $schema = get_schema('perl::modname', {ns_prefix=>'Graphics::ColorNames'});
+
+1;
+# ABSTRACT:
---
+++ lib/Sah/Schemas/GraphicsColorNames.pm
@@ -0,0 +1,11 @@
+package Sah::Schemas::GraphicsColorNames;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Sah schemas related to Graphics::ColorNames
+
+=head1 append:SEE ALSO
+
+L<Graphics::ColorNames>
