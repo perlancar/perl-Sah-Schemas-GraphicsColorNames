abe479530434e897b92c1d9dc883d5709a8580ab
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-20 07:14:16 +0700

    Add alias name Sah::Schemas::ColorScheme and mention about 'color theme' vs 'color scheme' terms

---
+++ lib/Sah/Schemas/ColorScheme.pm
@@ -0,0 +1,22 @@
+package Sah::Schemas::ColorScheme;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Alias for Sah::Schemas::GraphicsColorNames
+
+=head1 DESCRIPTION
+
+I use "color schemes" to refer to modules in the L<Graphics::ColorNames>::*
+namespace, while "color themes" to refer to modules in the L<ColorTheme>::* (or
+C<WHATEVER::ColorTheme::*>) namespace. The color theme modules allow defining
+color items that have background color (in addition to foreground color), as
+well as dynamic color (coderef).
+
+
+=head1 append:SEE ALSO
+
+L<Graphics::ColorNames>
+
+L<Sah::Schemas::ColorTheme> and L<ColorTheme>
