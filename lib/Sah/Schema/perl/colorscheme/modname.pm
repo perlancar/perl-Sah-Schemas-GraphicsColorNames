c02bbd206932f8c9a589666639326b5aa3b8d4bb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-20 05:34:00 +0700

    [doc] Add summary

--- lib/Sah/Schema/perl/colorscheme/modname.pm
+++ lib/Sah/Schema/perl/colorscheme/modname.pm
@@ -8,7 +8,11 @@ package Sah::Schema::perl::colorscheme::modname;
 use Sah::PSchema qw(get_schema);
 use Sah::Schema::perl::modname; # not detected yet
 
-our $schema = get_schema('perl::modname', {ns_prefix=>'Graphics::ColorNames'});
+our $schema = get_schema(
+    'perl::modname',
+    {ns_prefix=>'Graphics::ColorNames'},
+    {summary => 'Perl module in the Graphics::ColorNames:: namespace'},
+);
 
 1;
 # ABSTRACT:
